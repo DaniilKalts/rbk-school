033462a92e35213ab6ee0f369c3647afddb57bb0
DaniilKalts <kaltsdaniil@gmail.com>
2026-05-08 22:03:18 +0500

    feat(5-week): добавил лимит размера тела и запрет неизвестных полей в JSON

--- 5-week/internal/adapters/transport/http/helpers/request.go
+++ 5-week/internal/adapters/transport/http/helpers/request.go
@@ -2,13 +2,26 @@ package helpers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
+const maxRequestBodyBytes = 1 << 20
+
 func DecodeJSON(w http.ResponseWriter, r *http.Request, v any) bool {
-	if err := json.NewDecoder(r.Body).Decode(v); err != nil {
-		response := NewErrorResponse(http.StatusBadRequest, "некорректное тело запроса")
-		JSON(w, http.StatusBadRequest, response)
+	r.Body = http.MaxBytesReader(w, r.Body, maxRequestBodyBytes)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(v); err != nil {
+		var maxBytesErr *http.MaxBytesError
+		if errors.As(err, &maxBytesErr) {
+			JSON(w, http.StatusRequestEntityTooLarge, NewErrorResponse(http.StatusRequestEntityTooLarge, "тело запроса слишком большое"))
+			return false
+		}
+
+		JSON(w, http.StatusBadRequest, NewErrorResponse(http.StatusBadRequest, "некорректное тело запроса"))
 		return false
 	}
 
